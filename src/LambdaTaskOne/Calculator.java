1824ac54076ca15ee70c52316c05b55cf306ce22
Denis Chigvintsev <mainz24@yandex.ru>
2022-04-21 11:09:31 +0400

    Add corrections hw LambdaTaskOne

--- src/LambdaTaskOne/Calculator.java
+++ src/LambdaTaskOne/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
     BinaryOperator<Integer> devide = (x, y) -> x / y;
 
     UnaryOperator<Integer> pow = x -> x * x;
-    UnaryOperator<Integer> abs = x -> x > 0 ? x : x++;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x+1;
 
     Predicate<Integer> isPositive = x -> x > 0;
 
--- src/LambdaTaskOne/Main.java
+++ src/LambdaTaskOne/Main.java
@@ -6,13 +6,16 @@ public class Main {
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
 
+        a = calc.abs.apply(a);
+        b = calc.abs.apply(b);
+
         try {
             if (calc.isPositive.test(a & b)) { // Деление на 0 запрещено ArithmeticException
                 int c = calc.devide.apply(a, b);
                 calc.println.accept(c);
             }
         } catch (ArithmeticException e) {
-            calc.abs.apply(a & b); // Обработка тернарным оператором переменной со значением 0
+            e.getStackTrace();
         }
     }
 }
