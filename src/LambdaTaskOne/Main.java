02b8a34a4a118482c4282fb07eaa799d41bb3819
Denis Chigvintsev <mainz24@yandex.ru>
2022-05-04 19:31:57 +0400

    Add TaskOne 04.05

--- src/LambdaTaskOne/Main.java
+++ src/LambdaTaskOne/Main.java
@@ -6,16 +6,15 @@ public class Main {
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
 
-        a = calc.abs.apply(a);
-        b = calc.abs.apply(b);
-
+        int i = calc.abs.apply(a);
+        System.out.println(i);
+        int j = calc.abs.apply(b);
+        System.out.println(j);
         try {
-            if (calc.isPositive.test(a & b)) { // Деление на 0 запрещено ArithmeticException
-                int c = calc.devide.apply(a, b);
-                calc.println.accept(c);
-            }
+            int c = calc.devide.apply(i, j);
+            calc.println.accept(c);
         } catch (ArithmeticException e) {
-            e.getStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 }
